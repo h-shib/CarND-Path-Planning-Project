ead0ece88af4c02ade6380a402166a2cde94070b
h-shib <hiraku.shibuya888@gmail.com>
2017-08-24 06:59:56 +0900

    enable to slow down when other car is in front of ego car

--- src/main.cpp
+++ src/main.cpp
@@ -201,7 +201,7 @@ int main() {
   int lane = 1;
 
   // reference velocity to target
-  double ref_vel = 49.5;
+  double ref_vel = 0;
 
   h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -241,6 +241,38 @@ int main() {
             auto sensor_fusion = j[1]["sensor_fusion"];
 
             int prev_size = previous_path_x.size();
+
+
+            if (prev_size > 0) {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            // find ref_v to use
+            for (int i = 0; i < sensor_fusion.size(); i++) {
+              // car is in my lane
+              float d = sensor_fusion[i][6];
+              if (d < (4+lane*4) && d > (lane*4)) {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s += (double)prev_size * .02 * check_speed;
+
+                if ((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
+                  //ref_vel = 29.5;
+                  too_close = true;
+                }
+              }
+            }
+
+            if (too_close) {
+              ref_vel -= .224;
+            } else if (ref_vel < 49.5) {
+              ref_vel += .224;
+            }
 
 
 
